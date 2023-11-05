16b2dd1319461317e9a651ad977c9649b4850966
diabolik1975 <stefano.iaboni@gmail.com>
2023-11-05 11:15:40 +0100

    fuxbig #2

--- viaggiatreno.py
+++ viaggiatreno.py
@@ -33,42 +33,40 @@ for codiceTreno in treni:
         print("url =", url)
         # store the response of URL 
         response = urlopen(url) 
-        # storing the JSON response  
-        # from url in data 
-        data_json = json.loads(response.read())   
-        # print the json response 
-        #print(data_json) 
-        # Start from the cell . Rows and columns are zero indexed.
-        row = dataOdierna.day
-        col = 0
-        orarioDiPartenza = datetime.fromtimestamp(int(round(data_json['orarioPartenzaZero']/1000)))
-        #orarioDiPartenza = datetime.strptime(orarioDiPartenza, '%H:%M')
-        #print(int(round(data_json['orarioPartenzaZero']/1000)))
-        print("%s:%s" % (orarioDiPartenza.strftime("%H"), orarioDiPartenza.strftime("%M")))
-        worksheet = workbook.add_worksheet(codiceTreno+'_'+data_json['origine'][0:3]+'_'+data_json['destinazione'][0:3]+'_'+orarioDiPartenza.strftime("%H")+orarioDiPartenza.strftime("%M"))
-        worksheet.write(0,0, "Giorno")
-        worksheet.write(33,0, "Totale Ritardo")
-        worksheet.write(34,0, "Media Ritardo")
-        worksheet.write(row,col, dataOdierna, cell_date_format)
-        print("data odierna =", dataOdierna)
-        # Iterating through the json
-        # list
-        col=1
-        for fermate in data_json['fermate']:
-            print(str(fermate['stazione']) + "," + str(fermate['ritardo']))
-            worksheet.write(0,col, str(fermate['stazione']),cell_stazioni_format)
-            if (fermate['ritardo']>0):
-                cell_format_ritardo.set_bg_color('yellow')
-                worksheet.write(row,col, fermate['ritardo'],cell_format_ritardo)
-                print('yellow')
-            else:
-                cell_format_standard.set_bg_color(False)
-                worksheet.write(row,col, fermate['ritardo'],cell_format_standard)
-            column_letter= xlsxwriter.utility.xl_col_to_name(col)
-            print (column_letter)
-            worksheet.write(33,col, '=SUM(%s2:%s32)' %(column_letter,column_letter))
-            worksheet.write(34,col, '=AVERAGE(%s2:%s32)' %(column_letter,column_letter))
-            col = col+1
+        print(response.getcode())
+        if response.getcode()==200:
+            data_json = json.loads(response.read())   
+            #print(data_json) 
+            row = dataOdierna.day
+            col = 0
+            orarioDiPartenza = datetime.fromtimestamp(int(round(data_json['orarioPartenzaZero']/1000)))
+            print("%s:%s" % (orarioDiPartenza.strftime("%H"), orarioDiPartenza.strftime("%M")))
+            worksheet = workbook.add_worksheet(codiceTreno+'_'+data_json['origine'][0:3]+'_'+data_json['destinazione'][0:3]+'_'+orarioDiPartenza.strftime("%H")+orarioDiPartenza.strftime("%M"))
+            worksheet.write(0,0, "Giorno")
+            worksheet.write(33,0, "Totale Ritardo")
+            worksheet.write(34,0, "Media Ritardo")
+            worksheet.write(row,col, dataOdierna, cell_date_format)
+            print("data odierna =", dataOdierna)
+            # Iterating through the json
+            # list
+            col=1
+            for fermate in data_json['fermate']:
+                print(str(fermate['stazione']) + "," + str(fermate['ritardo']))
+                worksheet.write(0,col, str(fermate['stazione']),cell_stazioni_format)
+                if (fermate['ritardo']>0):
+                    cell_format_ritardo.set_bg_color('yellow')
+                    worksheet.write(row,col, fermate['ritardo'],cell_format_ritardo)
+                    print('yellow')
+                else:
+                    cell_format_standard.set_bg_color(False)
+                    worksheet.write(row,col, fermate['ritardo'],cell_format_standard)
+                column_letter= xlsxwriter.utility.xl_col_to_name(col)
+                print (column_letter)
+                worksheet.write(33,col, '=SUM(%s2:%s32)' %(column_letter,column_letter))
+                worksheet.write(34,col, '=AVERAGE(%s2:%s32)' %(column_letter,column_letter))
+                col = col+1
+        else:
+            print("Informazioni non disponibili per il treno "  +treni[codiceTreno]+ " e per il giorno " +str(dataOdierna))
     except:
         print("Si è verificato un errore nel recupero delle informazioni")
 workbook.close()   
